1f3a395e6b13d9d8ee533349ccf7d0eae7f34248
Jake Parks <jakob.parks@gmail.com>
2013-11-04 18:48:19 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScreenscrapeKata::Application.config.secret_key_base = '240cdd62e3cea9b49c55e2589fc21094ccc9d43258dfa594603072c387bf454763f62f12b8aff363641235a3f0406f57e68ff11ace37d5d5435ff19240c6392f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScreenscrapeKata::Application.config.session_store :cookie_store, key: '_screenscrape_kata_session'
